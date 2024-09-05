c57205e7dadcc401307ec682e64ae8282d9ed517
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-06 03:38:29 +0700

    Edit gradle build dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.wifi_scanner"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.wifi_scanner"
@@ -37,7 +37,8 @@ android {
 
 dependencies {
     implementation("com.google.android.gms:play-services-vision:20.1.3")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.9.2")
 }
