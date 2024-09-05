75c4bd61170b79bea371ff03b61b04a3392c8d53
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-06 03:22:40 +0700

    migrate from groovy to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.wifi_scanner"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.wifi_scanner"
+        minSdk = 28
+        //noinspection ExpiredTargetSdkVersion
+        targetSdk = 28
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    dataBinding {
+        enable = true
+    }
+}
+
+dependencies {
+    implementation("com.google.android.gms:play-services-vision:20.1.3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
