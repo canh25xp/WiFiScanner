7af04b5e9bddfc9a11f0c8e00ead6035f22cb396
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-19 09:09:56 +0700

    Add Password Activity layout

--- app/src/main/java/com/example/wifiscanner/PasswordActivity.java
+++ app/src/main/java/com/example/wifiscanner/PasswordActivity.java
@@ -1,30 +1,86 @@
 package com.example.wifiscanner;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.net.wifi.WifiNetworkSpecifier;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class PasswordActivity extends AppCompatActivity {
-
-    public static void connectToNetWork(String name, String s, Context mContext) {
-    }
+public class PasswordActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button;
+    EditText password;
+    String SSID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Toast.makeText(this, "TODO: Implement this", Toast.LENGTH_SHORT).show();
+
+        // Hide the action bar on top
+        if (getSupportActionBar() != null) this.getSupportActionBar().hide();
+
+        password = findViewById(R.id.input_password);
+        button = findViewById(R.id.connect_button);
+        button.setOnClickListener(this);
+        Bundle bundle = getIntent().getExtras();
+        TextView title = findViewById(R.id.connect_title);
+        assert bundle != null;
+        SSID = bundle.getString("NAME WIFI");
+        title.setText("Connect to " + SSID);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.connect_button) {
+            connectToNetWork(SSID, password.getText().toString(), this);
+            finish();
+        }
+    }
+
+    public static void connectToNetWork(String SSID, String password, Context mContext) {
+        NetworkRequest networkRequest = getNetworkRequest(SSID, password);
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            cm.requestNetwork(networkRequest, new ConnectivityManager.NetworkCallback() {
+                @Override
+                public void onAvailable(@NonNull Network network) {
+                    super.onAvailable(network);
+                    cm.bindProcessToNetwork(network);
+                }
+            });
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private static NetworkRequest getNetworkRequest(String SSID, String password) {
+        WifiNetworkSpecifier.Builder builder = new WifiNetworkSpecifier.Builder();
+        builder.setSsid(SSID);
+        builder.setWpa2Passphrase(password);
+        WifiNetworkSpecifier wifiNetworkSpecifier = builder.build();
+        NetworkRequest.Builder networkRequestBuilder = new NetworkRequest.Builder();
+        networkRequestBuilder.addTransportType(NetworkCapabilities.TRANSPORT_WIFI);
+        networkRequestBuilder.setNetworkSpecifier(wifiNetworkSpecifier);
+        return networkRequestBuilder.build();
     }
 }
